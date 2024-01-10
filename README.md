ef0cba55d18fe7d26ca77a982bc9967f952babf0
AldairZambrano <aldairzambrano12345@gmail.com>
2024-01-10 14:05:59 -0500

    Clon de netflix

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pagina clon de netflix
+
+
+
+Creada con fines didacticos y educativos.
+
+☣ **Esta página no es oficial** ☣
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+ <div className="login-container">
+    <h1>Sign In</h1>
+    <input type='text' placeholder='enter correo'></input>
+    <input type='password' placeholder='password'></input>
+    <button>Sign In</button>
+ </div>
+  )
+}
+
+export default App
